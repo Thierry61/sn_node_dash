d01d06060b318e8b5e26dc6f61795bf392ef2c02
Thierry61 <tfavier61@gmail.com>
2022-02-27 23:13:33 +0100

    Added dashboard export from InfluxDB Cloud

--- empty_bucket.sh
+++ empty_bucket.sh
@@ -1,3 +1,6 @@
+# - Works on local InfluxDB OSS
+# - Get "unauthorized access" message on InfluxDB cloud (changed host, org, token and removed port number)
+# => The simplest workaround in this case is to delete and recreate SN bucket in InfluxDB UI
 curl --request POST "http://localhost:8086/api/v2/delete?org=MyOrg&bucket=SN" \
   --header 'Authorization: Token GYQamYfyV46350EY0EBw7bVPUzVQGXz6qUEGkV3BFdchJJ0IPOn1xTpV-HkhJV3NaJ-Jb1MGi9w_Zk9gmcaL1w==' \
   --header 'Content-Type: application/json' \
